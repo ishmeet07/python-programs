8a815286fa94776fb42c4ed02f9bc100937fbe1f
ishmeet07 <84372705+ishmeet07@users.noreply.github.com>
2021-09-02 22:38:28 +0530

    Update arraylinearsearch.py

--- arraylinearsearch.py
+++ arraylinearsearch.py
@@ -4,10 +4,17 @@ y=int(input(("enter the no of elements")))
 for i in range(0,y):
     a= int(input(f"enter the element {i}"))
     x.append(a)
+x.sort()    
 a=arr.array('i',x)
 for i in range(0,y):
     print(a[i],end=" ")
 k=int(input("\nenter the element you want to search"))
+count=0
 for i in range(0,y):
     if(a[i]==k):
         print(f"element found at loc{i}")
+        count+=1
+    else:
+        pass
+if(count ==0):
+    print("no element found")    
